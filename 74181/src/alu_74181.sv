7ee8acfef471f3e40558b57c90c0f3d98531429a
Pedro Oliveira <ph.telecom@outlook.com>
2022-02-27 13:35:57 -0300

    [dev/74181] Added F outputs according to S selection input

--- 74181/src/alu_74181.sv
+++ 74181/src/alu_74181.sv
@@ -9,18 +9,61 @@
 
 
 module alu_74181(
-    input  logic   [3:0] S_selection_i,
-    input  logic         M_operation_i,
-    input  logic         Carry_i,
-    input  logic   [3:0] A_i,        // Active-high data inputs
-    input  logic   [3:0] B_i,        // Active-high data inputs
-    output logic         equality,   // A = B
-    output integer [3:0] F_number_o, // Number output
-    output logic         G_lookahead_o,
-    output logic         P_lookahead_o;
+    input  logic [3:0]  S_selection_i,
+    input  logic        mode_control_i,
+    input  logic        carry_in_i,
+    input  logic [3:0]  A_i,        // Active-high data inputs
+    input  logic [3:0]  B_i,        // Active-high data inputs
+    output logic        equality_o,   // A = B
+    output signed [3:0] F_o,         // Number output
+    output logic        carry_plus_four_o,
+    output logic        G_lookahead_o,
+    output logic        P_lookahead_o;
 );
 
-
+always @(*) begin
+    if (mode_control_i) begin // M = High: Perform Logic Operations
+        case(S_selection_i) begin
+            4'b0000: F_o = ~A_i;
+            4'b0001: F_o = ~(A_i | B_i);
+            4'b0010: F_o = ~A_i & B_i;
+            4'b0011: F_o = 4'b0;
+            4'b0100: F_o = ~(A_i & B_i);
+            4'b0101: F_o = ~B_i;
+            4'b0110: F_o = A_i ^ B_i;
+            4'b0111: F_o = A_i & ~B_i;
+            4'b1000: F_o = (~A_i & B_i);
+            4'b1001: F_o = ~(A_i ^ B_i);
+            4'b1010: F_o = B_i;
+            4'b1011: F_o = A_i & B_i;
+            4'b1100: F_o = 4'b1;
+            4'b1101: F_o = A_i | ~B_i;
+            4'b1110: F_o = A_i | B_i;
+            4'b1111: F_o = A_i;
+            default: F_o = 4'bX;
+        endcase
+    end else begin // M = Low: Perform Arithmetic Operations
+        case(S_selection_i) begin
+            4'b0000: F_o = carry_in_i ? (A_i + 1'b1) : A_i;
+            4'b0001: F_o = carry_in_i ? ((A_i | B_i) + 1'b1) : (A_i | B_i);
+            4'b0010: F_o = carry_in_i ? ((A_i | ~B_i) + 1'b1) : (A_i | ~B_i);
+            4'b0011: F_o = carry_in_i ? 4'b0 : 4'sb1001;
+            4'b0100: F_o = carry_in_i ? (A_i + (A_i & ~B_i) + 1'b1) : (A_i + (A_i & B_i));
+            4'b0101: F_o = carry_in_i ? ((A_i | B_i) + (A_i & ~B_i) + 1'b1) : ((A_i | B_i) + (A_i & ~B_i));
+            4'b0110: F_o = carry_in_i ? (A_i - B_i) : (A_i - B_i - 1'b1);
+            4'b0111: F_o = carry_in_i ? (A_i & ~B_i) : (A_i & ~B_i) - 1'b1;
+            4'b1000: F_o = carry_in_i ? (A_i + (A_i & B_i) + 1'b1) : (A_i + B_i);
+            4'b1001: F_o = carry_in_i ? (A_i + B_i + 1'b1) : (A_i + B_i);
+            4'b1010: F_o = carry_in_i ? ((A_i + ~B_i) + (A_i & B_i) + 1'b1) : ((A_i | ~B_i) + (A_i & B_i));
+            4'b1011: F_o = carry_in_i ? A_i & B_i : (A_i & B_i) - 1'b1;
+            4'b1100: F_o = carry_in_i ? (A_i + A_i + 1'b1) : (A_i + A_i);
+            4'b1101: F_o = carry_in_i ? ((A_i | B_i) + A_i + 1'b1) : (A_i | B_i) + A_i;
+            4'b1110: F_o = carry_in_i ? ((A_i | ~B_i) + A_i + 1'b1) : (A_i | ~B_i) + A_i;
+            4'b1111: F_o = carry_in_i ? A_i : A_i - 1'b1; 
+            default: F_o = 4'bX;
+        endcase
+    end
+end
 
 
 
