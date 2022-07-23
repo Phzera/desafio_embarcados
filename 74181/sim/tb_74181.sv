b061f143c24d2149539df1502675386aaa26a994
Pedro Oliveira <ph.telecom@outlook.com>
2022-07-23 13:25:33 -0300

    [dev/74181] Remove randomization

--- 74181/sim/tb_74181.sv
+++ 74181/sim/tb_74181.sv
@@ -15,7 +15,7 @@ module tb_74181;
     logic [3:0] tb_B;
     logic       clk;
            
-    dut alu_74181(
+    alu_74181 dut(
         .S_selection_i  (tb_selector),
         .mode_control_i (tb_M),
         .carry_in_i     (tb_carry),
@@ -28,36 +28,35 @@ module tb_74181;
     always #(CLK_PERIOD/2) clk=~clk;
     
     // Randomize inputs Selector, A and B
-    class Inputs;
-        randc  logic [3:0] s;
-        randc logic [3:0] a;
-        randc logic [3:0] b;
-    endclass
-    
-    assign tb_A        = rand_input.a;
-    assign tb_B        = rand_input.b;
-    assign tb_selector = rand_input.s;
-
     initial begin
+        $dumpvars;
         $dumpfile("tb_74181.vcd");
-        $dumpvars(0, tb_181);
     end
     
+    int i, j; //iterator
     initial begin
 
         #1 clk<=1'bx;
         #(CLK_PERIOD*3) clk<=0;
-
-        Inputs rand_input = new();
-        for (int i = 0; i < 16; i++) begin
-            rand_input.randomize.();
-            $display("Iteration= %0d, S= %0h, A = %0h, B = %0h", i, 
-                        rand_input.s, rand_input.a, rand_input.b);
-        end
+        tb_M <= 1'b1;
+        tb_selector <= 1'b0;
         
+        // Stimulus Logic operation
+        for (i = 0; i < 16; i = i++) begin
+            //rand_input.randomize.();
+            tb_selector = tb_selector + i;
+            //$display("Iteration = %0d, S = %0h, A = %0h, B = %0h", i, 
+            //             rand_input.s, rand_input.a, rand_input.b);
+            $display("Iteration = %0d, S = %0h", i, tb_selector);
+            //for (j = 0; )
+        end
+
+        #(CLK_PERIOD*3);
+        tb_M <= 1'b0;
+
        repeat(2) @(posedge clk);
 
-        $finish(2);
+      $finish(1);
     end
     
     endmodule
